7c03c278833d5ac4e4913ff28c0da44304a1d6cd
Deivisson Lisboa <dvilaflor@gmail.com>
2023-07-21 11:31:22 -0300

    Implement gameplay logic
    Add eventListiner for all button on the page and create logic
    to with the already made game loop

--- source/script.js
+++ source/script.js
@@ -1,13 +1,49 @@
-const CHOICES = ["rock", "paper", "scissors"]
-let score = 0
+const SCORE_DISPLAY = document.getElementById("score")
+const STATUS = document.getElementById("status")
+const PLAYER_DISPLAY = document.getElementById("player")
+const COMPUTER_DISPLAY = document.getElementById("computer")
+const ARENA = document.getElementById("arena")
+const SELECTION_BTNS = document.querySelectorAll(".selection")
+const PLAY_BTN = document.getElementById("play")
 
-function getComputerChoice() {
-  return CHOICES[Math.floor(Math.random() * CHOICES.length)]
+const SELECTION = ["rock", "paper", "scissors"]
+let playerSelection = ""
+let games = 0
+let score = 0
+let newGame = false
+
+function getComputerSelection() {
+  return SELECTION[Math.floor(Math.random() * SELECTION.length)]
 }
 
-function play() {
-  let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
-  let computerSelection = getComputerChoice()
+function setArenaStatus(status) {
+  ARENA.dataset.status = status
+}
+
+function resetArenaStatus() {
+  ARENA.dataset.status = ""
+}
+
+function setScore() {
+  score++
+  SCORE_DISPLAY.textContent = score
+}
+
+function resetScore() {
+  games = 0
+  score = 0
+  SCORE_DISPLAY.textContent = "0"
+}
+
+function setStatus(status) {
+  STATUS.textContent = status
+}
+
+function resetStatus() {
+  STATUS.textContent = `you need to win ${3 - score} more game(s)`
+}
+
+function play(playerSelection, computerSelection) {
   let win = false
 
   if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -17,24 +53,87 @@ function play() {
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     win = true
   } else if (playerSelection === computerSelection) {
-    console.log("DRAW! play again")
-    play()
+    setArenaStatus("tie")
+    setStatus("It's a tie")
+    return
   }
 
   if (win) {
-    score++
-    return `You Won! ${playerSelection} beats ${computerSelection}`
+    setScore()
+    setStatus(`You Won! ${playerSelection} beats ${computerSelection}`)
+    setArenaStatus("win")
+  } else if (computerSelection === "dragon") {
+    setStatus("You Lose! Dragon beats everything!")
+    setArenaStatus("lose")
   } else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
+    setStatus(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    setArenaStatus("lose")
   }
+
+  games++
+}
+
+function setPlayerSelection(selection) {
+  playerSelection = selection
+  PLAYER_DISPLAY.dataset.selection = selection
+}
+
+function resetPlayerSelection() {
+  playerSelection = ""
+  PLAYER_DISPLAY.dataset.selection = ""
+}
+
+function setComputerSelection(selection) {
+  COMPUTER_DISPLAY.dataset.selection = selection
+}
+
+function resetComputerSelection() {
+  COMPUTER_DISPLAY.dataset.selection = ""
 }
 
 function game() {
-  score = 0
-  for (let i = 0; i < 5; i++) {
-    console.log(play())
+  if (playerSelection) {
+    let computerSelection
+    if (games === 4 && score === 2) {
+      computerSelection = "dragon"
+    } else {
+      computerSelection = getComputerSelection()
+    }
+
+    play(playerSelection, computerSelection)
+
+    setComputerSelection(computerSelection)
+
+    if (score >= 3) {
+      PLAY_BTN.textContent = "victory"
+      newGame = true
+    } else if (games - score >= 3) {
+      PLAY_BTN.textContent = "game over"
+      newGame = true
+    }
+
+    PLAY_BTN.setAttribute("disabled", "")
   }
-  console.log(`Score: ${score}`)
 }
 
-// game()
+SELECTION_BTNS.forEach((selection) => {
+  selection.addEventListener("click", () => {
+    if (newGame) {
+      newGame = false
+      resetScore()
+      PLAY_BTN.textContent = "play"
+    }
+
+    resetArenaStatus()
+    resetComputerSelection()
+    resetStatus()
+
+    setPlayerSelection(selection.getAttribute("id"))
+
+    PLAY_BTN.removeAttribute("disabled")
+  })
+})
+
+PLAY_BTN.addEventListener("click", () => {
+  game()
+})
